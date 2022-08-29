91d1ae979a589552bf525c98aa94e58389f7b420
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-08-29 12:40:18 +0100

    routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,14 @@
 import {Router, Request, Response} from 'express';
-import { StatusCodes } from 'http-status-codes';
+import { register, login, logout } from '../controllers/auth'
 
 /* GET home page. */
 const routes = (route: Router) => {
   route.get('/api/v1',(req: Request, res: Response) => {
     res.json({msg: 'Connected!'})
   })
+  route.post('/api/v1/users/register', register)
+  route.post('/api/v1/users/login', login)
+  route.get('/api/v1/users/logout', logout)
 }
 
 export default routes;
