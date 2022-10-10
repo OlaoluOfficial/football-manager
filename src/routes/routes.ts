207739036b50a33198c54e9183d93503b16e293d
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-10-10 21:05:55 +0100

    add verifytoken middleware

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,14 +1,18 @@
-import {Router, Request, Response} from 'express';
-import { register, login, logout } from '../controllers/auth'
+import { Router, Request, Response } from 'express';
+import { register, login, logout } from '../controllers/auth';
+import verifyToken from '../middlewares/verifyToken';
+import { getPlayers, getPlayer } from '../controllers/players';
 
 /* GET home page. */
 const routes = (route: Router) => {
-  route.get('/api/v1',(req: Request, res: Response) => {
-    res.json({msg: 'Connected!'})
-  })
-  route.post('/api/v1/users/register', register)
-  route.post('/api/v1/users/login', login)
-  route.get('/api/v1/users/logout', logout)
-}
+  route.get('/api/v1', (req: Request, res: Response) => {
+    res.json({ msg: 'Connected!' });
+  });
+  route.post('/api/v1/users/register', register);
+  route.post('/api/v1/users/login', login);
+  route.get('/api/v1/users/logout', logout);
+  route.get('/api/v1/players', verifyToken, getPlayers);
+  route.get('/api/v1/players/:id', verifyToken, getPlayer);
+};
 
 export default routes;
