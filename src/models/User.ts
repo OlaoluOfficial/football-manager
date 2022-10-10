0f95ef0abbe927fd3db82aba016c7fac123fc463
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-10-10 21:05:40 +0100

    user model

---
+++ src/models/User.ts
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Please provide email'],
+  },
+  age: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  }
+})
+
+export default mongoose.model('User', UserSchema)
